854f783780b56a73f07e582e27153c27b47705df
James <jnotears@gmail.com>
2022-07-27 02:01:53 +0700

    apply ci/cd to deploy netlify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from "./app.component";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/module-schema' },
+  { path: '' , component: AppComponent},
   { path: 'module/:id', loadComponent: () => import('./module/module-detail.component').then(c => c.ModuleDetailComponent) }
 ];
 
