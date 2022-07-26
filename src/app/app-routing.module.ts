ba019ef78dafc17914e1504ce6201e2b8cf289cf
James <jnotears@gmail.com>
2022-07-27 02:26:53 +0700

    remove duplicate layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from "./app.component";
 
 const routes: Routes = [
-  { path: '' , component: AppComponent},
   { path: 'module/:id', loadComponent: () => import('./module/module-detail.component').then(c => c.ModuleDetailComponent) }
 ];
 
